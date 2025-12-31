72a89bc47526ba05973d4b010c79f9fda1387a3b
Mark Basaldua <markbasaldua@gmail.com>
2025-12-31 04:11:48 -0700

    chore: initial FloodForge iOS project scaffold

---
+++ FloodForge/FloodForge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FloodForge
+//
+//  Created by Mark Basaldua on 12/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FloodForge/FloodForge/FloodForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloodForgeApp.swift
+//  FloodForge
+//
+//  Created by Mark Basaldua on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct FloodForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
